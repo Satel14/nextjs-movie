985c2cbe5835a6b331f04de90a6d0935ea8d5d47
Satel14 <ostaplvov@gmail.com>
2023-03-10 01:30:18 +0200

    changed security settings

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    async headers() {
+      return [
+        {
+          source: '/',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "upgrade-insecure-requests"
+            },
+          ],
+        },
+      ]
+    },
   },
 }
 
