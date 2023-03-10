b50c3f456a383609180cff718999121ccdf1ec37
Satel14 <ostaplvov@gmail.com>
2023-03-10 11:29:04 +0200

    base api url changed

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    async headers() {
-      return [
-        {
-          source: '/',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "upgrade-insecure-requests"
-            },
-          ],
-        },
-      ]
-    },
   },
 }
 
